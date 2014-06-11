508e4ef804085494fdb8497fb757bb94847ee953
iamaya <imaya.kumar@yahoo.com>
2014-06-11 12:03:45 -0400

    Main

---
+++ App3/App3/App3/FirstPage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App3
+{
+    class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            BackgroundColor = Color.Red;
+
+            var btn =  new Button
+                    {
+                    HorizontalOptions = LayoutOptions.Start, Text = "Submit", Font = Font.OfSize("Helvetica",25), 
+                    BackgroundColor= Color.Red
+                    };
+
+            btn.Clicked +=   delegate 
+            {
+            DisplayAlert("Title","Message","OK",null);
+            };
+
+            Content = new StackLayout
+                {
+                    Padding = 20,
+                    BackgroundColor = Color.Silver,
+                    Orientation = StackOrientation.Vertical,
+                    VerticalOptions = LayoutOptions.Center,
+
+                    Children = {
+                    new Label
+                    {
+                     Text = "Login", Font = Font.OfSize("Helvetica",30), HorizontalOptions = LayoutOptions.Start, TextColor = Color.Red
+                    },
+
+                    new Entry
+                    {
+                        HorizontalOptions=LayoutOptions.StartAndExpand, Placeholder="User Id", WidthRequest= 350
+                    },
+
+                    new Entry
+                    {
+                        HorizontalOptions=LayoutOptions.StartAndExpand, Placeholder="Password", WidthRequest= 350, IsPassword = true,
+                        Keyboard = Keyboard.Numeric
+                    },
+
+                   btn
+                    }
+                };
+        }
+    }
+}
